6bc5acbfd4f27d8034c4b4e72b45473033eb47c7
link <pangzizhuo@hotmail.com>
2020-03-27 00:03:14 +0800

    配置celery

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask
+from celery import Celery
 
 
 def create_app(test_config=None):
@@ -10,8 +11,12 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite')
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        CELERY_BROKER_URL='redis://loaclhost:6379/0',
+        CELERY_RESULT_BACKEND='redis://localhost:6379/0'
     )
+    celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+    celery.conf.update(app.config)
 
     if test_config is None:
         app.config.from_pyfile('config.py', silent=True)
