ff6241a27b3e8f5f1932fa9ae2add51206f7be3e
link <pangzizhuo@hotmail.com>
2020-03-29 16:29:51 +0800

    注册celery函数

--- flaskr/celery_app/cele.py
+++ flaskr/celery_app/cele.py
@@ -6,7 +6,7 @@ celery_app = Celery(
     'flask_Celery',
     broker=flaskConfig.CELERY_BROKER_URL,
     backend=flaskConfig.CELERY_RESULT_BACKEND,
-    include=['flaskr.celery_app.celery_task.add']
+    include=['flaskr.celery_app.celery_task']
 )
 
 
